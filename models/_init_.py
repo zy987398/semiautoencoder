628fc840a6649dbcc32c66154edad00bb7141ad8
zy987398 <421097339@qq.com>
2025-06-05 10:43:42 +0800

    Update _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -1,5 +1,5 @@
 from .vae import VariationalAutoEncoder, VAETrainer
 from .ensemble import EnsembleUncertaintyEstimator
-
+from .autoencoder import 
 __all__ = ['VariationalAutoEncoder', 'VAETrainer', 'EnsembleUncertaintyEstimator']
 
